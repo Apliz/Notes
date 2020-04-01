c70ffd5e17be4ca39dd29773231e5ead67c7e56d
Anton Plisnier <anttongp@gmail.com>
2020-04-01 16:46:19 +0100

    step 3 complete; all tests passing]

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,10 +1,11 @@
-(function(exports) {
+(function (exports) {
 
   function testListView() {
     view = new noteListView();
     view.noteList.add("Favourite drink: seltzer");
     if (view.viewNotesAsHTML() == '<ul><li><div>Favourite drink: seltzer</ul></li></div>') {
       console.log('Note is represented in HTML :: Test Pass');
+      view.noteList.collection = [];
       return true;
     } else {
       console.log('Note is not represented in HTML :: Test Fail')
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,9 +1,12 @@
-(function(exports) {
+(function (exports) {
   function noteListView() {
     this.noteList = new listNoteObjects();
   }
   noteListView.prototype.viewNotesAsHTML = function () {
-    return "<ul><li><div>Favourite drink: seltzer</ul></li></div>"
+    for (let i = 0; i < this.noteList.collection.length; i++) {
+      console.log(`<ul><li><div>${this.noteList.collection[i].text}</ul></li></div>`);
+    };
+    if (this.noteList.collection.length == 0) { return "no notes to transform!" }
   };
   exports.noteListView = noteListView;
 })(this);
